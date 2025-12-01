4d31c90f97ccffb5174e22b360e677933306783c
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-01 11:32:02 -0500

    1st Function. Rectangle Parameters

--- Chapter 6 Project.cpp
+++ Chapter 6 Project.cpp
@@ -1,9 +1,26 @@
-// This is the Main Branch! Select the sub-branches to grade this project
+// This is the Rectangle SubBranch!
 
 #include <iostream>
 using namespace std;
 
+void getRectParameters(double &x, double &y) {  //This function recieves and validates the user-entered values for the Rectangle's length/width, then stores them for future use
+    do {
+        cout << "Enter the Rectangle's Length(must be greater than 0): ";
+        cin >> x;
+    } while (x < 0.01);
+    do {
+        cout << "Enter the Rectangle's Width(must be greater than 0): ";
+        cin >> y;
+    } while (y < 0.01);
+}
+
 int main()
 {
-    cout << "Hello World2!\n";
+    double rectLength = 0;
+    double rectWidth = 0;
+
+    getRectParameters(rectLength, rectWidth);
+
+    cout << rectLength;
+    cout << rectWidth;
 }
