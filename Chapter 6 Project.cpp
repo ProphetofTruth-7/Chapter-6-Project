6e792e44c721613d0dff93278db25c86fb6f7ea6
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-01 16:36:21 -0500

    checkpoint to look around

--- Chapter 6 Project.cpp
+++ Chapter 6 Project.cpp
@@ -3,7 +3,21 @@
 #include <iostream>
 using namespace std;
 
+void validateJudgeScore(double& x) {  //This function recieves and validates the user-entered values for the judge's score, then stores them for future use
+    do {
+        cout << "Enter the Judge's score: ";
+        cin >> x;
+    } while (x < 0.01 || x > 10);
+}
+
 int main()
 {
-    cout << "Hello WorldStars!\n";
+    int judgeCounter = 1;
+    int judge1Score, judge2Score, judge3Score, judge4Score, judge5Score;
+
+
+    do {
+        validateJudgeScore;
+        ++judgeCounter;
+    } while (judgeCounter != 5);
 }
