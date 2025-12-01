afbb6a11c1d7347af9555ee29da1c20ee0db99bd
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-01 15:04:38 -0500

    Give up for now

--- Chapter 6 Project.cpp
+++ Chapter 6 Project.cpp
@@ -14,40 +14,41 @@ void getRegionInfo(int &x) {
 
 int main()
 {
-    int northAcc = 0, southAcc = 0, eastAcc = 0, westAcc = 0, centralAcc = 0, regionCounter = 1;
+    int northAcc = -1, southAcc = -1, eastAcc = -1, westAcc = -1, centralAcc = -1;
+    int regionCounter = 1;
     string currentRegion;
 
     do {
         cout << "What region's accidents are we storing?(North, South, East, West, or Central): ";
         cin >> currentRegion;
 
-        if (currentRegion == "North") {
+        if (currentRegion == "North" && northAcc > -1) {
             getRegionInfo(northAcc);
             ++regionCounter;
             cout << endl;
         }
-        else if (currentRegion == "South") {
+        else if (currentRegion == "South" && southAcc > -1) {
             getRegionInfo(southAcc);
             ++regionCounter;
             cout << endl;
         }
-        else if (currentRegion == "East") {
+        else if (currentRegion == "East" && eastAcc > -1) {
             getRegionInfo(eastAcc);
             ++regionCounter;
             cout << endl;
         }
-        else if (currentRegion == "West") {
+        else if (currentRegion == "West" && westAcc > -1) {
             getRegionInfo(westAcc);
             ++regionCounter;
             cout << endl;
         }
-        else if (currentRegion == "Central") {
+        else if (currentRegion == "Central" && centralAcc > -1) {
             getRegionInfo(centralAcc);
             ++regionCounter;
             cout << endl;
         }
         else {
-            cout << "You entered an invalid region \n";
+            cout << "You entered an invalid region or one you already did \n";
         }
     } while (regionCounter < 6);
 
