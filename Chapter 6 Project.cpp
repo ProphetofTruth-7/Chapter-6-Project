96d5a649d5f0ce3821fce918a2aabb4698b183dd
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-01 18:26:58 -0500

    Not quite complete

--- Chapter 6 Project.cpp
+++ Chapter 6 Project.cpp
@@ -19,6 +19,7 @@ void validateJudgeScore(double& x) {  //This function recieves and validates the
 int main()
 {
     int judgeCounter = 1;
+    double averageScore;
     double judge1Score, judge2Score, judge3Score, judge4Score, judge5Score;
 
 
@@ -40,4 +41,63 @@ int main()
         }
         ++judgeCounter;
     } while (judgeCounter != 6);
+
+    double leastScore = judge1Score;
+    double greatestScore = judge1Score;
+
+    if (leastScore <= judge5Score) {       // This nest assigns the leastScore for future use
+        if (leastScore <= judge4Score) {
+            if (leastScore <= judge3Score) {
+                if (leastScore <= judge2Score) {
+                    if (leastScore <= judge1Score) {
+                        leastScore = judge1Score;
+                    }
+                }
+                else {
+                    leastScore = judge2Score;
+                }
+            }
+            else {
+                leastScore = judge3Score;
+            }
+        }
+        else {
+            leastScore = judge4Score;
+        }
+    }
+    else {
+        leastScore = judge5Score;
+    }
+
+    if (greatestScore >= judge5Score) {     // This nest assigns the greatestScore for future use
+        if (greatestScore >= judge4Score) {
+            if (greatestScore >= judge3Score) {
+                if (greatestScore >= judge2Score) {
+                    if (greatestScore >= judge1Score) {
+                        greatestScore = judge1Score;
+                    }
+                }
+                else {
+                    greatestScore = judge2Score;
+                }
+            }
+            else {
+                greatestScore = judge3Score;
+            }
+        }
+        else {
+            greatestScore = judge4Score;
+        }
+    }
+    else {
+        greatestScore = judge5Score;
+    }
+
+    averageScore = ((judge1Score + judge2Score + judge3Score + judge4Score + judge5Score) - (leastScore + greatestScore)) / 3;
+
+    cout << leastScore << endl;
+    cout << greatestScore << endl;
+    cout << averageScore << endl;
+    
+    return 0;
 }
