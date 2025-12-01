381bacaa947ea194a85c47c6b4c00559f30e0520
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-01 16:45:37 -0500

    Valid Score

--- Chapter 6 Project.cpp
+++ Chapter 6 Project.cpp
@@ -7,17 +7,37 @@ void validateJudgeScore(double& x) {  //This function recieves and validates the
     do {
         cout << "Enter the Judge's score: ";
         cin >> x;
+        if (x < 0.01 || x > 10) {
+            cout << "\nInvalid Score. Enter a valid score between 0 and 10: ";
+        }
+        else {
+            
+        }
     } while (x < 0.01 || x > 10);
 }
 
 int main()
 {
     int judgeCounter = 1;
-    int judge1Score, judge2Score, judge3Score, judge4Score, judge5Score;
+    double judge1Score, judge2Score, judge3Score, judge4Score, judge5Score;
 
 
     do {
-        validateJudgeScore;
+        if (judgeCounter == 1) {
+            validateJudgeScore(judge1Score);
+        }
+        else if (judgeCounter == 2) {
+            validateJudgeScore(judge2Score);
+        }
+        else if (judgeCounter == 3) {
+            validateJudgeScore(judge3Score);
+        }
+        else if (judgeCounter == 4) {
+            validateJudgeScore(judge4Score);
+        }
+        else {
+            validateJudgeScore(judge5Score);
+        }
         ++judgeCounter;
-    } while (judgeCounter != 5);
+    } while (judgeCounter != 6);
 }
