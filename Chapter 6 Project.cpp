072629a3c574761aed052436428000c593bb14c6
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-01 14:51:49 -0500

    Long elseif

--- Chapter 6 Project.cpp
+++ Chapter 6 Project.cpp
@@ -1,9 +1,56 @@
-// This is the Main Branch! Select the sub-branches to grade this project
+// This is the Safe Driving Sub-branch!
 
 #include <iostream>
+#include <string>
 using namespace std;
+
+void getRegionInfo(int &x) {
+    do {
+        cout << "How many accidents did this region experience in the past year?: ";
+        cin >> x;
+    } while (x < 0);
+}
+
 
 int main()
 {
-    cout << "Hello World3!\n";
+    int northAcc = 0, southAcc = 0, eastAcc = 0, westAcc = 0, centralAcc = 0, regionCounter = 1;
+    string currentRegion;
+
+    do {
+        cout << "What region's accidents are we storing?(North, South, East, West, or Central): ";
+        cin >> currentRegion;
+
+        if (currentRegion == "North") {
+            getRegionInfo(northAcc);
+            ++regionCounter;
+            cout << endl;
+        }
+        else if (currentRegion == "South") {
+            getRegionInfo(southAcc);
+            ++regionCounter;
+            cout << endl;
+        }
+        else if (currentRegion == "East") {
+            getRegionInfo(eastAcc);
+            ++regionCounter;
+            cout << endl;
+        }
+        else if (currentRegion == "West") {
+            getRegionInfo(westAcc);
+            ++regionCounter;
+            cout << endl;
+        }
+        else if (currentRegion == "Central") {
+            getRegionInfo(centralAcc);
+            ++regionCounter;
+            cout << endl;
+        }
+        else {
+            cout << "You entered an invalid region \n";
+        }
+    } while (regionCounter < 6);
+
+    cout << northAcc;
+    cout << centralAcc;
 }
