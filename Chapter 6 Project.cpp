5925aaf75ba4028ea131683488719233d811e2fb
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-02 11:29:07 -0500

    small fix

--- Chapter 6 Project.cpp
+++ Chapter 6 Project.cpp
@@ -5,16 +5,13 @@
 using namespace std;
 
 double calcCelsius(double x) {   //This function calculates the Celsius formula and returns the value for use in the table. It only requires a fahrenheit value
-    return 0.5555 * (x-32);
+    return 0.55555556 * (x-32);
 }
 
 
 int main()
 {
     const int MINTEMP = 0, MAXTEMP = 20;  // This sets your range for the minimum and maximum Fahrenheit. Change these to make a larger range
-    int loopIncrement = 1;
-
-    cout << endl;
 
     cout << "            Conversion Table" << endl;
     cout << "Fahrenheit" << setw(26) << "Celsius" << endl;
