56a72ec68885e11ea4a0390f3bfb388ab71648bd
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-01 21:42:44 -0500

    checkpoint2

--- Chapter 6 Project.cpp
+++ Chapter 6 Project.cpp
@@ -3,13 +3,25 @@
 #include <iostream>
 using namespace std;
 
-void judgeNumberVerification
+void judgeNumberVerification(int& x) {  //This function recieves and validates the user-entered values for the number of rooms, then stores them for future use
+    do {
+        cout << "Enter the number of Judge(s): ";
+        cin >> x;
+        if (x < 0) {
+            cout << "\nInvalid number of Judges(must be greater than 0) \n";
+        }
+        else {
+
+        }
+    } while (x < 0);
+}
 
 int main()
 {
     double leastScore, greatestScore, judgeScore, scoreTotal = 0, scoreAvg;
     int numberOfJudges;
 
+    judgeNumberVerification(numberofJudges);
     cout << "How many judges are there?: " << endl;
     cin >> numberOfJudges;
 
