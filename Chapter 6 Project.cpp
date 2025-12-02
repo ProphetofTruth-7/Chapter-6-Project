5c9099efcfe0ec6ff86aa7614548447be1312bb1
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-02 10:13:16 -0500

    Checkpoint. Going to School

--- Chapter 6 Project.cpp
+++ Chapter 6 Project.cpp
@@ -1,9 +1,24 @@
-// This is the Main Branch! Select the sub-branches to grade this project
+// This is the Celsius Table Subbranch!
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+double calcCelsius(double x) {   //This function calculates the Celsius formula and returns the value for use in the table
+    return 0.5555 * (x-32);
+}
+
 
 int main()
 {
-    cout << "Hello Wor4ld!\n";
+    const int MINTEMP = 0, MAXTEMP = 20;  // This sets your range for the minimum and maximum Fahrenheit. Change these to make a larger range
+    int fahrenheitValue;
+
+    do {
+        cout << "Enter the Fahrenheit(must be an integer between " << MINTEMP << " and " << MAXTEMP << "): ";
+        cin >> fahrenheitValue;
+        cout << "The Celsius value of that temperature is: " << calcCelsius(fahrenheitValue);
+    } while (fahrenheitValue > MAXTEMP || fahrenheitValue < MINTEMP);
+
+
 }
