c59a084dd2876c1aba374f3da999e90d9dacd3de
Malakye Brown <prophetoftruth07@gmail.com>
2025-12-02 11:47:41 -0500

    broke

--- Chapter 6 Project.cpp
+++ Chapter 6 Project.cpp
@@ -3,8 +3,31 @@
 #include <iostream>
 using namespace std;
 
+void regionInfo(int& x) {
+    do {
+        cout << "(How many accidents(0+) were in this region?): ";
+        cin >> x;
+        if (x < 0) {
+            cout << "Invalid Value" << endl;
+        }
+    } while (x < 0);
+}
 
 int main()
 {
-    
+    int northAcc, southAcc, eastAcc, westAcc, centralAcc;
+
+    cout << "North";
+    regionInfo(northAcc);
+    cout << "South";
+    regionInfo(southAcc);
+    cout << "East";
+    regionInfo(eastAcc);
+    cout << "West";
+    regionInfo(westAcc);
+    cout << "Central";
+    regionInfo(centralAcc);
+
+    cout << northAcc;
+    return 0;
 }
